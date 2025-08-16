038b2b95a52e60174dba0687f52f335af8146007
Gayatri-k3 <gayatrik1029@gmail.com>
2025-08-16 12:24:01 +0530

    Update and delete operation through controller

--- Tourism/src/main/java/com/xworkz/tourism/controller/TourismController.java
+++ Tourism/src/main/java/com/xworkz/tourism/controller/TourismController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.jws.WebParam;
 import java.util.List;
 import java.util.Optional;
 
